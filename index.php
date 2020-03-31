93141d48c45341f898af186ce363dbc092b0df0c
Fandomicals <fandomicals@gmail.com>
2020-03-31 23:19:36 +0800

    Update index.php

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ wp_reset_query();?>
                     <h3 class="text-center article-title">LATEST ARTICLES</h3>
                 </div>
                 <div class="col-md-4">
-                    <img class="img-responsive" alt=“portait” src="images/img1.jpg">
+                    <img class="img-responsive" alt=“portrait” src="http://206.189.45.97/~mesh22/sub3/wp-content/themes/starter_for_josh_site/images/img1.jpg">
                     <h4>A PORTRAIT</h4>
                     <p class="date">12 December 2020</p>
                     <p>STS has never been shy of seeking new terrains of investigation. More and more STS s cholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
@@ -35,7 +35,7 @@ wp_reset_query();?>
                     <p class="readmore">CONTINUE READING</p>
                 </div>
                 <div class="col-md-4">
-                    <img class="img-responsive" alt=“portait” src="images/img2.jpg">
+                    <img class="img-responsive" alt=“portrait” src="http://206.189.45.97/~mesh22/sub3/wp-content/themes/starter_for_josh_site/images/img2.jpg">
                     <h4>A PORTRAIT</h4>
                     <p class="date">12 December 2020</p>
                     <p>STS has never been shy of seeking new terrains of investigation. More and more STS s cholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
@@ -43,7 +43,7 @@ wp_reset_query();?>
                     <p class="readmore">CONTINUE READING</p>
                 </div>
                 <div class="col-md-4">
-                    <img class="img-responsive" alt=“portait” src="images/img3.jpg">
+                    <img class="img-responsive" alt=“portrait” src="http://206.189.45.97/~mesh22/sub3/wp-content/themes/starter_for_josh_site/images/img3.jpg">
                     <h4>A PORTRAIT</h4>
                     <p class="date">12 December 2020</p>
                     <p>STS has never been shy of seeking new terrains of investigation. More and more STS s cholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
