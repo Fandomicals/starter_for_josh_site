6e647a8d2d16964bcd0f253d36c9f608ea50f96d
Fandomicals <fandomicals@gmail.com>
2020-04-23 19:04:29 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -6,6 +6,9 @@
 <?php get_header(); /* Tells WordPress to include header.php */ ?>
           <section class="container-fluid aboutbg text-center">
         <div class="container">
+  <h2><?php the_field('titleforaboutme'); ?></h2><!--  custom field for the aboutme content-->
+            <p class="particle"><?php the_field('subtitleforaboutme'); ?></p>
+<p class="about-text"><?php the_field('paragraphforaboutme'); ?></p>
 
             <div class="blocker"></div>
 
@@ -16,13 +19,25 @@
         <div class="container">
             <div class="row"><!--  a row that gives us access to the BS columns-->
                 <div>
-                    <h3 class="text-center article-title">LATEST ARTICLES</h3>
+                    <h3 class="text-center article-title "><?php the_field('titleforarticles'); ?></h3>
                 </div>
                 <div class="col-md-4">
+                      <img class="img-responsive" alt=“portrait” src="<?php the_field('articleimage'); ?>">
+                      <h4><?php the_field('articletitle'); ?></h4>
+                    <p><?php the_field('articleexcerpt'); ?> </p>
+                     <a href="<?php the_field('readmore'); ?>" class="readmore">CONTINUE READING</a>
                 </div>
                 <div class="col-md-4">
+                    <img class="img-responsive" alt=“model” src="<?php the_field('articleimagetwo'); ?>">
+                      <h4><?php the_field('articletitletwo'); ?></h4>
+                    <p><?php the_field('articleexcerpttwo'); ?> </p>
+                     <a href="<?php the_field('readmoretwo'); ?>" class="readmore">CONTINUE READING</a>
                 </div>
-                <div class="col-md-4">   
+                <div class="col-md-4">
+                    <img class="img-responsive" alt=“racetrack” src="<?php the_field('articleimagethree'); ?>">
+                      <h4><?php the_field('articletitlethree'); ?></h4>
+                    <p><?php the_field('articleexcerptthree'); ?> </p>
+                     <a href="<?php the_field('readmorethree'); ?>" class="readmore">CONTINUE READING</a>
                 </div>
                 </div>
             </div>
